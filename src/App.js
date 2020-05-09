b0ef3a4fe427adc13f7f78a965846fe89ef0c586
08Gauri <mail.gauri08@gmail.com>
2020-05-10 02:20:42 +0530

    Added style to body and scroll to cardlist

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import React ,{Component} from 'react';
 import {Robots} from './Robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-
-
+import './App.css';
+import Scroll from './Scroll';
 
 class App extends Component{
 	constructor(){
 		super();
 		this.state = {
-			robots : Robots,
+			robots : [],
 			searchfield :''
 		}
 	}
 
+componentDidMount(){
+	fetch('https://jsonplaceholder.typicode.com/users')
+	.then(response => response.json())
+	.then(users => this.setState({robots : users}));
+}
 
 onEventChange = (event) => {
 	this.setState({searchfield : event.target.value});
@@ -24,13 +29,21 @@ onEventChange = (event) => {
 		const filteredRobots = this.state.robots.filter(robots =>{
 		return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 	});
-		return(
-			<div className = 'tc'>
-			<h1> ROBOFRIENDS </h1>
-			<SearchBox searchChange = {this.onEventChange}/>
-			<CardList robots = {filteredRobots} />
-			</div>
-			);
+		if(this.state.robots.length === 0){
+			return <h1>Loading . . . </h1>
+		}
+
+		else{
+			return(
+				<div className = 'tc'>
+				<h1> ROBOFRIENDS </h1>
+				<SearchBox searchChange = {this.onEventChange}/>
+				<Scroll>
+					<CardList robots = {filteredRobots} />
+				</Scroll>
+				</div>
+				);
+		}		
 	}
 }
 
